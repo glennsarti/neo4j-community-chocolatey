37ec8069d7e7a8a9c1f6344a8c9d24b82426e0a9
Glenn Sarti <glenn.sarti@outlook.com>
2015-03-26 22:10:41 +0800

    Added a template for Neo 2.2.0

---
+++ templates/package-neo4j-community-2.2.0.ps1
@@ -0,0 +1,9 @@
+$PackageDefinition = @{
+  "TemplateName" = "neo4j-community";
+  "PackageName" = "neo4j-community";
+  "PackageVersion" = "2.2.0";
+  "DownloadURL" = "http://neo4j.com/artifact.php?name=neo4j-community-2.2.0-windows.zip";
+  "MD5Checksum" = "9fa871bcec2525e578a598669483f570";
+  "NeoZipSubdir" = "neo4j-community-2.2.0";
+  "NeoServerApiJarSuffix" = "2.2.0";
+}
