09267a4c412c8c0716eaf128a7c865be1ac24ce3
Glenn Sarti <glennsarti@users.noreply.github.com>
2016-04-10 11:07:23 -0700

    Updated 2.3.0 with Java 8 template

---
+++ neo4j-community-2.3.0/tools/chocolateyUninstall.ps1
@@ -0,0 +1,32 @@
+$PackageName = 'neo4j-community'
+try {
+  # Get the NeoHome Dir
+  #   Try the local environment
+  $neoHome = [string] (Get-EnvironmentVariable -Name 'NEO4J_HOME' -Scope 'Machine')  
+  # Failing that, try a registry hack
+  if ($neoHome -eq '')
+  {
+    $neoHome = [string] ( (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment' -ErrorAction Continue).'NEO4J_HOME' )
+  }
+  if ($neoHome -eq '') { throw "Could not find the Neo4jHome directory" }
+
+  # Uninstall the Neo4j Service
+  $UninstallBatch = "$($neoHome)\bin\Neo4jInstaller.bat"
+  if (!(Test-Path $UninstallBatch)) { throw "Could not find the Neo4j Uninstaller Batch file at $UninstallBatch" }
+  
+  # Uninstall the service
+  $args = "remove"
+  $results = (Start-Process -FilePath $UninstallBatch -ArgumentList $args -Wait -PassThru -NoNewWindow)
+
+  # Remove the install folder
+  Remove-Item -Path $neoHome -Recurse -Force | Out-Null
+  
+  # Remove the environment variable
+  Install-ChocolateyEnvironmentVariable "NEO4J_HOME" '' "Machine"
+
+  Write-ChocolateySuccess $PackageName
+} catch {
+  Write-ChocolateyFailure $PackageName "$($_.Exception.Message)"
+  throw
+}
+
---
+++ templates/package-neo4j-community-2.3.0.ps1
@@ -0,0 +1,9 @@
+$PackageDefinition = @{
+  "TemplateName" = "neo4j-community8";
+  "PackageName" = "neo4j-community";
+  "PackageVersion" = "2.3.0.20151026";
+  "DownloadURL" = "http://neo4j.com/artifact.php?name=neo4j-community-2.3.0-windows.zip";
+  "MD5Checksum" = "70fee0ead6ed53db5d527afc25f00d14";
+  "NeoZipSubdir" = "neo4j-community-2.3.0";
+  "NeoServerApiJarSuffix" = "2.3.0";
+}
