59e07b90ffeba705768de466213e1725388e4065
Glenn Sarti <glennsarti@users.noreply.github.com>
2017-03-29 18:01:36 +0000

    New Packages added by Appveyor 2017-03-29-18:01:36+00:00

---
+++ templates/package-neo4j-community-3.1.3.ps1
@@ -0,0 +1,11 @@
+$PackageDefinition = @{
+  "TemplateName" = "neo4j-community-v3.1";
+  "PackageName" = "neo4j-community";
+  "PackageVersion" = "3.1.3";
+  "DownloadURL" = "http://neo4j.com/artifact.php?name=neo4j-community-3.1.3-windows.zip";
+  "MD5Checksum" = "32ee25959824a3cc0fd4658674b4e122";
+  "NeoZipSubdir" = "neo4j-community-3.1.3";
+  "NeoServerApiJarSuffix" = "3.1.3";
+  "PrivateJavaVersion" = "8.0.92.14";
+  "PrivateJreChecksumMD5" = "a852c7c6195e2ff8d0f0582d4d12a9b0";
+}
