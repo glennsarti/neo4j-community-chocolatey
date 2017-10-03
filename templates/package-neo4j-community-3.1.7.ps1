5486972f7bc008c1c3701763900d1fde0129fe6b
Glenn Sarti <glennsarti@users.noreply.github.com>
2017-10-03 18:01:44 +0000

    New Packages added by Appveyor 2017-10-03-18:01:44+00:00

---
+++ templates/package-neo4j-community-3.1.7.ps1
@@ -0,0 +1,12 @@
+$PackageDefinition = @{
+  "TemplateName" = "neo4j-community-v3.1";
+  "PackageName" = "neo4j-community";
+  "PackageVersion" = "3.1.7";
+  "DownloadURL" = "http://neo4j.com/artifact.php?name=neo4j-community-3.1.7-windows.zip";
+  "MD5Checksum" = "0c1679b2c230550d8bc04493784cd721";
+  "NeoZipSubdir" = "neo4j-community-3.1.7";
+  "NeoServerApiJarSuffix" = "3.1.7";
+  "PrivateJavaVersion" = "8.0.131.11";
+  "PrivateJavaURIDir" = "d54c1d3a095b4ff2b6607d096fa80163/";
+  "PrivateJreChecksumMD5" = "9458b62000daac0f48155323185f1c4c";
+}
---
+++ templates/package-neo4j-community-3.2.6.ps1
@@ -0,0 +1,12 @@
+$PackageDefinition = @{
+  "TemplateName" = "neo4j-community-v3.1";
+  "PackageName" = "neo4j-community";
+  "PackageVersion" = "3.2.6";
+  "DownloadURL" = "http://neo4j.com/artifact.php?name=neo4j-community-3.2.6-windows.zip";
+  "MD5Checksum" = "34c68763d4ee5c71c39e5b1ff6e98797";
+  "NeoZipSubdir" = "neo4j-community-3.2.6";
+  "NeoServerApiJarSuffix" = "3.2.6";
+  "PrivateJavaVersion" = "8.0.131.11";
+  "PrivateJavaURIDir" = "d54c1d3a095b4ff2b6607d096fa80163/";
+  "PrivateJreChecksumMD5" = "9458b62000daac0f48155323185f1c4c";
+}
