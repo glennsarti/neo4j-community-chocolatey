5ce815ca32218337ada92c911963c27c2863fccb
Glenn Sarti <glennsarti@users.noreply.github.com>
2018-04-18 03:00:38 +0000

    New Packages added by Appveyor 2018-04-18-03:00:38+00:00

---
+++ templates/package-neo4j-community-3.4.0-beta02-beta.ps1
@@ -0,0 +1,12 @@
+$PackageDefinition = @{
+  "TemplateName" = "neo4j-community-v3.1-beta";
+  "PackageName" = "neo4j-community";
+  "PackageVersion" = "3.4.0-beta02-beta";
+  "DownloadURL" = "http://neo4j.com/artifact.php?name=neo4j-community-3.4.0-beta02-windows.zip";
+  "MD5Checksum" = "da2f583111d2a06f66248a385cf895b6";
+  "NeoZipSubdir" = "neo4j-community-3.4.0-beta02";
+  "NeoServerApiJarSuffix" = "3.4.0-beta02";
+  "PrivateJavaVersion" = "8.0.131.11";
+  "PrivateJavaURIDir" = "d54c1d3a095b4ff2b6607d096fa80163/";
+  "PrivateJreChecksumMD5" = "9458b62000daac0f48155323185f1c4c";
+}
